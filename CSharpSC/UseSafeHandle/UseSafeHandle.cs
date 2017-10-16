543778c3b8d4fcb713f9f921529fc423f02543c8
rcseacord <rcseacord@gmail.com>
2017-10-16 12:30:21 -0400

    updated solution

--- CSharpSC/UseSafeHandle/UseSafeHandle.cs
+++ CSharpSC/UseSafeHandle/UseSafeHandle.cs
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+
 using System;
 using System.Globalization;
 using System.IO;
@@ -184,99 +185,177 @@ namespace UseSafeHandle
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
-public class DisposableStreamResource : IDisposable
+[assembly: CLSCompliant(true)]
+namespace UseSafeHandle
 {
-  // Define constants.
-  internal const uint GENERIC_READ = 0x80000000;
-  internal const uint FILE_SHARE_READ = 0x00000001;
-  internal const uint OPEN_EXISTING = 3;
-  internal const uint FILE_ATTRIBUTE_NORMAL = 0x80;
-  internal const int INVALID_FILE_SIZE = unchecked((int)0xFFFFFFFF);
-
-  [SuppressUnmanagedCodeSecurity]
-  internal static class NativeMethods
-  {
-    // Define Windows APIs.
-    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-    internal static extern SafeFileHandle CreateFile(string lpFileName, uint dwDesiredAccess,
-        uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition,
-        uint dwFlagsAndAttributes, IntPtr hTemplateFile
-    );
-
-    [DllImport("kernel32.dll")]
-    internal static extern int GetFileSize(SafeFileHandle hFile, out int lpFileSizeHigh);
-  }
-  // Define locals.
-  private bool disposed = false;
-  private SafeFileHandle safeFileHandle;
-  private long bufferSize;
-  private int upperWord;
-
-  public DisposableStreamResource(string fileName)
-  {
-    if (String.IsNullOrWhiteSpace(fileName))
+    public class DisposableStreamResource : IDisposable
     {
-      throw new ArgumentException(message: "The filename cannot be null, empty, or whitespace.", paramName: nameof(fileName));
-    }
+        // Define constants.
+        internal const uint GenericRead = 0x80000000;
+        internal const uint FileShareRead = 0x00000001;
+        internal const uint OpenExisting = 3;
+        internal const uint FileAttributeNormal = 0x80;
+        internal IntPtr InvalidHandleValue = new IntPtr(-1);
+        internal const int InvalidFileSize = unchecked((int)0xFFFFFFFF);
 
-    safeFileHandle = NativeMethods.CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ,
-                               IntPtr.Zero, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL,
-                               IntPtr.Zero);
-    if (safeFileHandle.IsInvalid)
-    {
-      throw new FileNotFoundException(
-        String.Format(CultureInfo.CurrentCulture, "Cannot open '{0}'", fileName),
-        new System.ComponentModel.Win32Exception());  // calls Marshal.GetLastWin32Error
-    }
+        [SuppressUnmanagedCodeSecurity]
+        internal static class NativeMethods
+        {
+            // Define Windows APIs.
+            [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+            internal static extern SafeFileHandle CreateFile(string lpFileName, uint dwDesiredAccess,
+                uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition,
+                uint dwFlagsAndAttributes, IntPtr hTemplateFile
+            );
 
-    // Get file size.
-    bufferSize = NativeMethods.GetFileSize(this.safeFileHandle, out upperWord);
-    if (bufferSize == INVALID_FILE_SIZE)
-      bufferSize = -1;
-    else if (upperWord > 0)
-      bufferSize = (((long)upperWord) << 32) + bufferSize;
-  }
+            [DllImport("kernel32.dll")]
+            internal static extern int GetFileSize(SafeFileHandle hFile, out int lpFileSizeHigh);
+        }
 
-  public long Size
-  { get { return bufferSize; } }
+        // Define locals.
+        private bool _disposed;
+        private readonly SafeFileHandle _safeHandle;
+        private readonly long _bufferSize;
 
-  public void Dispose()
-  {
-    Dispose(true);
-    GC.SuppressFinalize(this);
-  }
+        public DisposableStreamResource(string fileName)
+        {
+            if (String.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException(message: "The filename cannot be null, empty, or whitespace.", paramName: nameof(fileName));
+            }
 
-  protected virtual void Dispose(bool disposing)
-  {
-    if (disposed) return;
+            _safeHandle = NativeMethods.CreateFile(fileName, GenericRead, FileShareRead,
+                IntPtr.Zero, OpenExisting, FileAttributeNormal,
+                IntPtr.Zero);
+            if (_safeHandle.IsInvalid)
+            {
+                throw new FileNotFoundException(
+                    String.Format(CultureInfo.CurrentCulture, "Cannot open '{0}'", fileName),
+                    new System.ComponentModel.Win32Exception());  // calls Marshal.GetLastWin32Error
+            }
 
-    // Dispose of managed resources here.
-    if (disposing) safeFileHandle?.Dispose();
+            // Get file size.
+            _bufferSize = NativeMethods.GetFileSize(this._safeHandle, out int upperWord);
+            if (_bufferSize == InvalidFileSize)
+            {
+                _bufferSize = -1;
+            }
+            else if (upperWord > 0)
+                _bufferSize = (((long)upperWord) << 32) + _bufferSize;
+        }
 
-    // Dispose of any unmanaged resources not wrapped in safe handles.
+        public long Size => _bufferSize;
 
-    disposed = true;  
-  }
-}
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            // Dispose of managed resources here.
+            if (disposing)
+                _safeHandle.Dispose();
+
+            // Dispose of any unmanaged resources not wrapped in safe handles.
+
+            _disposed = true;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "System.Console.WriteLine(System.String)")]
+        public static void Main()
+        {
+            using (DisposableStreamResource dsr = new DisposableStreamResource(@"..\..\file.txt"))
+            {
+                Console.WriteLine("File Size = " + dsr.Size);
+                // Keep the console window open in debug mode.
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    } // end public class DisposableStreamResource
+} // end namespace SecureCSharp
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
